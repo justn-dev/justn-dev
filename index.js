79ccc4bc0ee988662be797ea37848aae4be6963f
Nờ <114742329+Xaydeptrai@users.noreply.github.com>
2024-10-30 18:35:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,44 +14,25 @@ const QUOTES_API = "https://zenquotes.io/api/quotes";
   const { quote, author } = await getQuotes(hour);
 
   const text = `### ${greetings}
-  <h2>I'm Michael, a Backend Engineer. <img src="https://media.giphy.com/media/mGcNjsfWAjY5AEZNw6/giphy.gif" width="50"></h2>
+  <h2>I'm N, a Backend Engineer. <img src="https://media.giphy.com/media/mGcNjsfWAjY5AEZNw6/giphy.gif" width="50"></h2>
 
   ## <img src="https://emojis.slackmojis.com/emojis/images/1588315024/8823/hyperkitty.gif?1588315024" width="30" /> SKILL
-  [<img align="right" width="50%" src="https://github-readme-stats.vercel.app/api?username=Kenini1805&show_icons=true&theme=synthwave">](https://metrics.lecoq.io/ouuan?template=classic)
+  [<img align="right" width="50%" src="https://github-readme-stats.vercel.app/api?username=Xaydeptrai&show_icons=true&theme=synthwave">](https://metrics.lecoq.io/ouuan?template=classic)
   
   - Understands \`OOP\` well
   - \`HTML\`, \`CSS\`, \`Bootstrap\`: proficient
-  - Strong skills in \`JQuery\`, \`MySQL\`, \`PHP\`
-  - Excellent with the \`Laravel\` framework for web design
+  - Strong skills in \`JavaScript\`, \`C#\`, \`MS-SQL\`
+  - Excellent with the \`Express\` framework for web design
   - Proficient with \`Git\`
-  - Learning the \`Agile\` mindset
-  - Knowledge of \`Vue.js\` and \`Vuex\`
+  - Knowledge of \`Node.js\`
   - Skill in \`Unit Testing\`
-  - Experience in manual and \`automated deployment\`
-  - Familiar with \`AWS\`
   
-  ## <img src="https://emojis.slackmojis.com/emojis/images/1643515721/17468/homersimpson-pbjdance.gif?1643515721" width="30" /> CERTIFICATES
   <img src="https://images.viblo.asia/1f5d99d1-8cb7-4d82-a627-d6934d20d94b.png" width="100" />
   
   ## <img src="https://images.viblo.asia/a22cc9ed-e446-4eae-ad55-1ddf8afbaa54.gif" width="30" /> CONTRIBUTIONS
-  [<img align="right" width="50%" src="https://github-readme-stats.vercel.app/api/top-langs/?username=Kenini1805&show_icons=true&theme=synthwave&layout=compact">](https://metrics.lecoq.io/ouuan?template=classic)
-  
-  #### 06/2020
-  **Contributor at Laravel**: 
-  - https://github.com/laravel/framework/pull/33278
-  - https://github.com/laravel/framework/pull/49669
-
-  #### 2019
-  **Contributor at Chat++**: 
-  - https://github.com/wataridori/chatpp/graphs/contributors
-  
-  #### Present
-  Owner of packages \`Laravel monitoring\` and \`Nginx monitoring\`
-  - https://github.com/AvengersCodeLovers/laravel-log-monitoring
-  - https://github.com/AvengersCodeLovers/nginx-log-monitoring
+  [<img align="right" width="50%" src="https://github-readme-stats.vercel.app/api/top-langs/?username=Xaydeptrai&show_icons=true&theme=synthwave&layout=compact">](https://metrics.lecoq.io/ouuan?template=classic)
   
   ## <img src="https://i.imgur.com/g4uAchW.gif" width="30" /> ABOUT ME
-  💬 Ask me anything: [chillwithsu.com](https://chillwithsu.com/)
   ## Quote of the day:
   *"${quote}"* <br>
   — ${author}
